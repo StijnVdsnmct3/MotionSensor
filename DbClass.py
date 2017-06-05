91f7bb9fab05a038ff71d7f03ae7827cd904d849
StijnVdsnmct3 <stijn.van.der.spiegel@student.Howest.be>
2017-06-05 17:24:01 +0200

    Dbfile edited/ basishtml/ home login page/ macros

--- DbClass.py
+++ DbClass.py
@@ -3,19 +3,45 @@ class DbClass:
         import mysql.connector as connector
 
         self.__dsn = {
-            "host": "HostnameHere",
-            "user": "UserNameHere",
-            "passwd": "PasswdHere",
-            "db": "DBNameHere"
+            "host": "localhost",
+            "user": "root",
+            "passwd": "Derter7027-",
+            "db": "homesecurity"
         }
 
         self.__connection = connector.connect(**self.__dsn)
         self.__cursor = self.__connection.cursor()
 
-    def getDataFromDatabase(self):
+    def getlocaties(self):
         # Query zonder parameters
-        sqlQuery = "SELECT * FROM tablename"
-        
+        sqlQuery = "SELECT * FROM locatie"
+
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+    def getlogs(self):
+        # Query zonder parameters
+        sqlQuery = "SELECT * FROM logs"
+
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def gettypes(self):
+        # Query zonder parameters
+        sqlQuery = "SELECT * FROM types"
+
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def getusers(self):
+        # Query zonder parameters
+        sqlQuery = "SELECT * FROM users"
+
         self.__cursor.execute(sqlQuery)
         result = self.__cursor.fetchall()
         self.__cursor.close()
@@ -32,11 +58,23 @@ class DbClass:
         self.__cursor.close()
         return result
 
-    def setDataToDatabase(self, value1):
+    def getuser(self, username, password):
         # Query met parameters
-        sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
+        sqlQuery = "SELECT * FROM users WHERE Username = '{param1}' and Password = '{param2}' "
         # Combineren van de query en parameter
-        sqlCommand = sqlQuery.format(param1=value1)
+        sqlCommand = sqlQuery.format(param1=username, param2=password)
+
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+
+    def setcomments(self, Naam, Voornaam, bericht):
+        # Query met parameters
+        sqlQuery = "INSERT INTO comments (Naam, Voornaam, Bericht) VALUES ('{param1}','{param2}','{param3}')"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=Naam, param2=Voornaam, param3=bericht)
 
         self.__cursor.execute(sqlCommand)
         self.__connection.commit()
--- route.py
+++ route.py
@@ -1,11 +1,33 @@
 from flask import Flask
+from flask import render_template
+from DbClass import DbClass
+
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['post'])
+def login():
+    error = None
+    from flask import request
+    if request.method == 'post':
+        username = request.form['uname']
+        password = request.form['psw']
+
+        do = DbClass()
+
+        result = do.getuser(username,password)
+
+        if result:
+            return render_template('home.hmtl', role=result[3])
+        else:
+            error = "Verkeerde Login"
+    render_template('login.html',error=error)
+
+@app.route('/Home')
+def Homepage():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
