3bbb5b87185e9692dfb5288a42be52c2f8acb0ed
StijnVdsnmct3 <stijn.van.der.spiegel@student.Howest.be>
2017-06-08 23:02:49 +0200

    data senden werkt/ motion werkt / datbase insert werkt/ tweede pi socket werkt

--- createlogs.py
+++ createlogs.py
@@ -6,6 +6,8 @@ from datetime import datetime
 import os.path
 import socket
 
+
+
 camera = PiCamera()
 
 sensor = 22
@@ -13,33 +15,41 @@ video = False
 foto = True
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup (sensor, GPIO.IN)
+GPIO.setup(sensor, GPIO.IN)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('192.168.1.99', 9050))
 
+
+def motion(video, foto):
+    if foto == True:
+        print("zit in foto")
+        save_path = '/home/pi/project1/static/Files'
+        filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S.jpg")
+        complete_path = os.path.join(save_path, filename)
+        camera.capture(filename)
+        print("nu database")
+        do = DbClass()
+        do.insertlog(1, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 1, filename)
+    if video == True:
+        save_path = '/home/pi/project1/static/Files'
+        filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S.h264")
+        complete_path = os.path.join(save_path, filename)
+        camera.start_recording(filename)
+        camera.wait_recording(5)
+        camera.stop_recording()
+        do = DbClass()
+        do.insertlog(1, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 2, filename)
 
 #locaties living=1 keuken=2 gang=3
 while True:
-    if sensor == 1:
-        beweging = True
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect('192.168.1.73', 9050)
-        sock.sendall(beweging)
-        ok = sock.recv(1024)
-        if ok == True:
-            sock.close()
-        if foto == True:
-            save_path = '/home/pi/flask/static/Files'
-            filename = datetime.now().strftime("%Y-%m-%d %H:%M:%S.JPEG")
-            complete_path = os.path.join(save_path, filename)
-            camera.capture(filename)
-            do = DbClass()
-            do.insertlog(1,datetime.now().strftime('%Y-%m-%d %H:%M:%S'),1,filename)
-        if video == True:
-            save_path = '/home/pi/flask/static/Files'
-            filename = datetime.now().strftime("%Y-%m-%d %H:%M:%S.h264")
-            complete_path = os.path.join(save_path, filename)
-            camera.split_recording(filename)
-            time.sleep(5)
-            camera.stop_recording()
-            do = DbClass()
-            do.insertlog(1, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 2, filename)
-
+    try:
+        #check voor beweging
+        GPIO.wait_for_edge(sensor, GPIO.RISING)
+        data = 'beweging'
+        sock.sendall(data.encode('utf-8'))
+        print("beweging verstuurd")
+        motion(video,foto)
+        time.sleep(10)
+    except:
+        camera.close()
+        GPIO.cleanup()
